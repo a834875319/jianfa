8eb630d645d06f6bdb5a6b74ee6436db97a42381
a834875319 <834875319@qq.com>
2018-10-18 17:07:30 +0800

    sijianshi

--- demo.java
+++ demo.java
@@ -3,5 +3,6 @@ public class Demo{
 		System.out.println("独孤九剑！一剑式！亢龙有悔！");
 		System.out.println("独孤九剑！二剑式！亢龙无悔！");
 		System.out.println("独孤九剑！三剑式！横扫千军！");
+                System.out.println("独孤九剑！四剑式！神龙摆尾！");
 	}
 }
